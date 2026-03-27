423d033e4099b0c78910ce50b818db7b6b89bda7
umakaran uma <umakaranuma1126@gmail.com>
2026-03-28 00:10:38 +0530

    Update ConfigurationScreen to ignore unused fields and elements
    - Added ignore directives for unused fields and elements to clean up the code and reduce warnings.
    - This change prepares the ConfigurationScreen for future enhancements while maintaining current functionality.

--- lib/screens/configuration_screen.dart
+++ lib/screens/configuration_screen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: unused_field, unused_element, deprecated_member_use
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:money_man/services/theme_service.dart';
